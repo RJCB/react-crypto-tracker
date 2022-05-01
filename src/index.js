a1254f35ce05eb940924c15434b2ece2de581977
RJCB <37097058+RJCB@users.noreply.github.com>
2022-05-01 12:56:29 -0400

    add comment for browser router

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <BrowserRouter basename="/react-crypto-tracker"> must be used if we want to deploy to Github pages,seems like base-url issue when deployed
   <BrowserRouter basename="/react-crypto-tracker">
     <App />
   </BrowserRouter>
